3a75757ac8252a64e5ea52a42fcf607f289c8361
Jacqueline <kok.j@hsleiden.nl>
2014-02-04 10:35:33 +0100

    added license

--- src/main/java/nl/hsleiden/authorizationservices/v1/services/AccessTokenFacadeREST.java
+++ src/main/java/nl/hsleiden/authorizationservices/v1/services/AccessTokenFacadeREST.java
@@ -1,6 +1,17 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2014 Hogeschool Leiden.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package nl.hsleiden.authorizationservices.v1.services;
 
@@ -21,7 +32,6 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
 import nl.hsleiden.authorizationservices.model.AccessToken;
